c01a1b5b0526a80b9fc79909fbe33425e5afefab
ANTONIO JARBAS RODRIGUES MARINHO <58714728+jarbasmarinho33@users.noreply.github.com>
2022-04-12 20:49:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let p = prompt("Seu nome")
+let t = prompt("Sua idade")
+//alert(typeof `${t,p}`)
+alert("Olá, " + p + "   você tem " + t + " anos de idade  Agora queremos lhe conhecer melhorJarbas.")
+//Usando o comando prompt capturei as variaveis e imprimi na tela usando o comando alert
+
+
+//alert(Vamos a outras peguntas para comcluir)
+let x = prompt("vc gosta de churros Sim ou Não?")
+let y = prompt("Vc estuda PHP Sim ou Não?")
+let z = prompt("Vc Digitou mais de 10 letras hoje Sim ou Não?")
+//alert(typeof `${x,z}`)
+alert("Suas resposta são as seguintes; vc gosta de churros: " + x + " Vc estuda PH: " + y + " Vc Digitou mais de 10 letras hoje: "  + z)
+//Usando o comando prompt capturei as variaveis e imprimi na tela usando o comando alert
+
+alert("Continuando nossa tarefa...")
+
+let a = 10
+let b = 25
+let a1 = b
+let b1 = a
+// Aqui faremos uma lógica para trocar os valores
+
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", a1) // O novo valor de a é 25
+console.log("O novo valor de b é", b1) // O novo valor de b é 10
+alert("Tecle F12 e veja seu consola agora")
