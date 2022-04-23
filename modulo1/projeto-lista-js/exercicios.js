db7a10ab84175e3195b32962d183f0478cdf35b9
ANTONIO JARBAS RODRIGUES MARINHO <jarbasmarinho33@gmail.com>
2022-04-22 23:08:18 -0300

    Exercicio imcompleto entrega até domingo 24/04/22

--- modulo1/projeto-lista-js/exercicios.js
+++ modulo1/projeto-lista-js/exercicios.js
@@ -19,41 +19,56 @@ function imprimeMensagem() {
 // EXERCÍCIO 01
 function calculaAreaRetangulo() {
   // implemente sua lógica aqui
- 
-  return altura * largura;
-
-
+  let altura = Number(prompt("informe a altura"));
+  let largura = Number(prompt("informe a largura"));
+  
+  let soma1 = altura * largura;
+  console.log(soma1)
 }
 
 // EXERCÍCIO 02
 function imprimeIdade() {
   // implemente sua lógica aqui
-
+  let atual = Number(prompt("informe a altura"))
+  let nascimento = Number(prompt("informe a largura"))
+  
+  let idade1 = atual - nascimento;
+  console.log(idade1)
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
-
+  let IMC  = peso / (altura * altura);
+  return(IMC)
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
-
+let nome  = prompt("Qual seu nome?") 
+let idade = prompt("Qual seu idade?") 
+let email = prompt("Qual seu email?") 
+console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
 }
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
+let cor1 = prompt("Escolha uma cor")
+let cor2 = prompt("Escolha uma cor")
+let cor3 = prompt("Escolha uma cor")
+
+console.log([cor1,cor2,cor3])
 
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
-
+  
+console.log(string.toUpperCase)
 }
 
 // EXERCÍCIO 07
--- modulo1/projeto-lista-js/teste2.js
+++
@@ -1,7 +0,0 @@
-let frase ="Meu cachorro biu";
-console.log(frase.toUpperCase);
-console.log(frase.replaceAll("a","i"));
-console.log(frase.replaceAll("e","i"));
-console.log(frase.replaceAll("o","i"));
-console.log(frase.replaceAll("u","i"));
-console.log(frase.length);
