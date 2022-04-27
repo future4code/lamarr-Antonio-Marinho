98c647d7f4e2640014830cb488e0038d7ac4d125
ANTONIO JARBAS RODRIGUES MARINHO <jarbasmarinho33@gmail.com>
2022-04-27 13:31:48 -0300

    atualizando exercício

--- modulo1/objetos/index.js
+++ modulo1/objetos/index.js
@@ -1,10 +1,28 @@
-const pessoa = {
+const pessoa1 = {
 nome: "Jon",
 Apelido: ["Magaiver", "Tito", "Mame"]
-
 }
-function myfunc(obj){
-let arrayinfo = [pessoa]
 
+function myfunc(pessoa1){
+
+    return (`Eu sou ${pessoa1.nome}, mas pode me chamar de: ${pessoa1.Apelido}`)
 }
-console.log(myfunc(pessoa))
+console.log(myfunc(pessoa1))
+
+const pessoa2 ={
+    nome : "Bruno",
+    idade : 23,
+    profissao : "Instrutor"
+}
+const pessoa3 ={
+    nome : "Jackson",
+    idade : 23,
+    profissao : "Contador"
+}
+function exibirinfo(){
+let pessoas = [pessoa2,pessoa3]
+let info = pessoa2.nome.length
+let info2 = pessoa3.nome.length
+return pessoas
+}
+console.log(exibirinfo())
