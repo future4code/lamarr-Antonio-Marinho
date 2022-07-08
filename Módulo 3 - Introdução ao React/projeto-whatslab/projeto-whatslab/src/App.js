057f7a7dbe8dda2a48d90c9ab3d1bb1ed38ff703
ANTONIO JARBAS RODRIGUES MARINHO <jarbasmarinho33@gmail.com>
2022-07-07 22:16:21 -0300

    whatslab

---
+++ Módulo 3 - Introdução ao React/projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div className="quadro-Msn">
+  
+          <form>
+            <label>Nome</label>
+            <input>
+            
+            </input>
+            
+            <label>Nome</label>
+            <input>
+            
+            </input>
+          </form>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Módulo 3 - Introdução ao React/projeto-whatslab/projeto-whatslab/src/Components/Inputs-msn.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function dadoform(){
+    return(
+        <>
+        <form>
+            <label> Quem</label>
+            <input
+            placeholder='Quem?'
+            
+            ></input>
+        </form>
+        </>
+    )
+}
+
+export default dadoform
